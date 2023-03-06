d6d10da4079edc998a59d3315316772eed125164
catfrd <100594122+catfrd@users.noreply.github.com>
2023-03-06 18:08:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import string as s
+from random import *
+
+ch=s.ascii_letters + s.digits + s.punctuation
+password="".join(choice(ch) for j in range(randint(8,16)))
+
+print("generated Password is " +password)
